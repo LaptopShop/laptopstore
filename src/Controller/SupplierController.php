33a9ff973fa0caa735ea0fe05716a524ad8b35c8
lamphat123 <108394322+lamphat123@users.noreply.github.com>
2022-07-14 22:40:47 +0700

    DeleteSupplierFeature

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -89,4 +89,20 @@ class SupplierController extends AbstractController
             'form' => $supplierForm->createView()
         ]);
     }
+      /**
+     * @Route("/delete/{id}", name="deleteSupplier")
+     */
+    public function deleteSupplierFunction(SupplierRepository $repo, ManagerRegistry $doc, $id): Response
+    {
+        $supplier = $repo->find($id);
+ 
+        if (!$supplier) {
+            throw
+            $this->createNotFoundException('Invalid ID ' . $id);
+        }
+        $entity = $doc->getManager();
+        $entity->remove($supplier);
+        $entity->flush();
+        return $this->redirectToRoute("app_supplier");
+    }
 }
